4dce83009b8ecc1360e0813babc9072bb1a8855b
Andy Dufilie <andy.dufilie@gmail.com>
2018-08-05 01:32:59 -0700

    Dancing Penguin

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,7 @@
 import { app, BrowserWindow } from "electron";
 import * as path from "path";
 
-let mainWindow: Electron.BrowserWindow;
-
-function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    height: 600,
-    width: 800,
-  });
-
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../index.html"));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
-  // Emitted when the window is closed.
-  mainWindow.on("closed", () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
-}
+let mainWindow: Electron.BrowserWindow | null;
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -32,20 +10,49 @@ app.on("ready", createWindow);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
 });
 
 app.on("activate", () => {
-  // On OS X it"s common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow();
+	}
 });
 
-// In this file you can include the rest of your app"s specific main process
+function createWindow() {
+	// Create the browser window.
+	mainWindow = new BrowserWindow({
+		height: 50,
+		width: 50,
+		useContentSize: true,
+		title: "Dancing Penguin",
+		frame: false,
+		transparent: true,
+		resizable: false,
+		alwaysOnTop: true,
+		skipTaskbar: true,
+	});
+
+	// and load the index.html of the app.
+	mainWindow.loadFile(path.join(__dirname, "../index.html"));
+
+	// Open the DevTools.
+	// mainWindow.webContents.openDevTools();
+
+	// Emitted when the window is closed.
+	mainWindow.on("closed", () => {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null;
+	});
+}
+
+// In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
